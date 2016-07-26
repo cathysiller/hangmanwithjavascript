6af4515f3f377e125f2afd1ecf145f442303a3ee
Cathy Siller <cathysiller503@gmail.com>
2016-07-26 12:32:13 -0500

    Add files via upload

--- myscripts.js
+++ myscripts.js
@@ -211,9 +211,9 @@ window.onload = function () {
     hint.onclick = function() {
 
       hints = [
-        ["Based in Mersyside", "Based in Mersyside", "First Welsh team to reach the Premier Leauge", "Owned by A russian Billionaire", "Once managed by Phil Brown", "2013 FA Cup runners up", "Gazza's first club"],
-        ["Science-Fiction horror film", "1971 American action film", "Historical drama", "Anamated Fish", "Giant great white shark"],
-        ["Northern city in the UK", "Home of AC and Inter", "Spanish capital", "Netherlands capital", "Czech Republic capital"]
+        ["chubby and cute", "odie", "small and real-life", "alice", "pink", "silent film", "japanese"],
+        ["Billy Crystal", "Julia Roberts", "Audrey Hepburn", "Prince Humperdinck", "Robbie Hart"],
+        ["Martin Luther King", "wintermoon", "animal", "riverside","blankwood"]
     ];
 
     var catagoryIndex = categories.indexOf(chosenCategory);
